44909ea277357f314736602d1f31d823b724d6b7
RISHU BAJPAI <83057142+rishubajpai007@users.noreply.github.com>
2023-09-27 19:45:17 +0530

    fixed image not loading problem

--- src/context/Context.js
+++ src/context/Context.js
@@ -6,11 +6,13 @@ const Cart = createContext();
 faker.seed(99);
 
 const Context = ({ children }) => {
+
+   const category = ["electronics", "clothing", "books", "toys", "home"];
   const products = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
     name: faker.commerce.productName(),
     price: faker.commerce.price(),
-    image: faker.random.image(),
+    image: `https://picsum.photos/1920/1080/?random=${Math.random()}&category=${category}`,
     inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
     fastDelivery: faker.datatype.boolean(),
     ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
